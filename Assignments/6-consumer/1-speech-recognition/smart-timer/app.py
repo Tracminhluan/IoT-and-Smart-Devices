62effbfa390bf234f41b81a16a2cebbc36b75f53
Trắc Minh Luận <tracminhluantv@gmail.com>
2026-04-10 18:45:30 +0700

    Add files via upload

---
+++ Assignments/6-consumer/1-speech-recognition/smart-timer/app.py
@@ -0,0 +1,26 @@
+import requests
+import time
+from azure.cognitiveservices.speech import SpeechConfig, SpeechRecognizer
+
+speech_api_key = 'key'
+location = 'southeastasia'
+language = 'en-GB'
+
+recognizer_config = SpeechConfig(subscription=speech_api_key,
+                                 region=location,
+                                 speech_recognition_language=language)
+
+recognizer = SpeechRecognizer(speech_config=recognizer_config)
+
+def process_text(text):
+    print(text)
+
+def recognized(args):
+    process_text(args.result.text)
+
+recognizer.recognized.connect(recognized)
+
+recognizer.start_continuous_recognition()
+
+while True:
+    time.sleep(1)
